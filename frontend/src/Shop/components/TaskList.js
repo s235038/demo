ae7103dd446cb7458cf074ec1422701bc5d9e58b
s235038 <s235038@yahoo.com.tw>
2025-02-19 08:17:42 +0800

    更新 TaskList.js

--- frontend/src/Shop/components/TaskList.js
+++ frontend/src/Shop/components/TaskList.js
@@ -1,5 +1,10 @@
 // TaskList.tsx
-import React from 'react';
+import React from '// 在任务列表上方添加分类按钮
+<div className="categories">
+  <button onClick={() => filterTasks('健康')}>健康</button>
+  <button onClick={() => filterTasks('教育')}>教育</button>
+  <button onClick={() => filterTasks('创意')}>创意</button>
+</div>';
 
 const TaskList = () => {
   return (
